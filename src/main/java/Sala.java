707961db62b2c40287eefff2d1c583fb667d6976
Przemek <przemyslawjakubowski@gmail.com>
2022-08-08 09:33:47 +0200

    task

--- src/main/java/Sala.java
+++ src/main/java/Sala.java
@@ -9,5 +9,31 @@ public class Sala {
         miejsca[2] = new Miejsce();
         miejsca[3] = new Miejsce();
         miejsca[4] = new Miejsce();
+
+        this.numerSali = numerSali;
+    }
+
+
+    public void rezerwujMiejsce() {
+        for (int i = 0; i < miejsca.length; i++) {
+            if (!miejsca[i].zajete) {
+                miejsca[i].zajete = true;
+                System.out.println("Zarezerwowano miejsce numer " + i + " na sali " + numerSali);
+                break;
+            }
+        }
+    }
+
+    public void zwolnijMiejsce(int numerMiejsca) {
+        if(numerMiejsca >= miejsca.length) {
+            System.out.println("Nie ma takiego miejsca");
+        } else {
+            if (miejsca[numerMiejsca].zajete) {
+                miejsca[numerMiejsca].zajete = false;
+                System.out.println("Zwalniam miejsce " + numerMiejsca);
+            } else {
+                System.out.println("Miejsce nie bylo zajete");
+            }
+        }
     }
 }
